c3aa5173fbc39aba26db2717bd69903d8bcd682f
Ali Merkouk <ali.merkouk@gmail.com>
2022-09-06 21:46:55 +0200

    good stuffs

---
+++ grid.js
@@ -0,0 +1,11 @@
+import { Grid } from "gridjs";
+import "gridjs/dist/theme/mermaid.css";
+
+new Grid({
+    data: [
+      ['John', 'john@example.com', '(353) 01 222 3333'],
+      ['Mark', 'mark@gmail.com',   '(01) 22 888 4444'],
+      ['Eoin', 'eo3n@yahoo.com',   '(05) 10 878 5554'],
+      ['Nisen', 'nis900@gmail.com',   '313 333 1923']
+    ]
+  });
--- ss.js
+++ ss.js
@@ -11,3 +11,44 @@ function makeRows(rows, cols) {
 };
 
 makeRows(7, 5);
+
+
+fetch("/Users/alimerkouk/Desktop/s2/recreate this/list.json")
+.then((res)=>res.json())
+.then(collectEach); 
+
+
+function collectEach(data){
+data.forEach(showCategory); 
+
+}
+
+
+function showCategory(oneCategory){
+
+
+
+
+    const template = document.querySelector("template").content; 
+
+    // Clone it 
+
+    const copy = template.cloneNode(true); 
+
+
+copy.querySelector("a").textContent=oneCategory.category;
+copy.querySelector("a").href= `/Users/alimerkouk/Desktop/product/product-list.html?category=${oneCategory.category}`;
+
+
+    // grab parent
+    const parent=document.querySelector("#letter_b ol"); 
+// Append
+
+parent.appendChild(copy);
+
+
+console.log(oneCategory);
+
+
+}
+
